017ce5e5847d4d10b96569bd27eb5aa6004ee8e1
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-13 23:15:15 +0530

    ADDED: Print Wins or Loss

--- src/com/blz/gambling/Gambler.java
+++ src/com/blz/gambling/Gambler.java
@@ -6,14 +6,21 @@ public class Gambler {
         int goal = 200;
         int bet = 1;
         int days = 0;
+        int wins = 0;
+        int losses = 0;
+
        for (int i = 0; i < 100 && stake < goal; i++);
-            days++;
             stake = stake - bet;
             boolean win = Math.random() < 0.5;
             if (win) {
                 stake = stake + bet;
+                wins++;
+            }else {
+                losses++;
+
             }
-        System.out.println("Days taken to reach goal: " + days);
+        System.out.println(" Wins " +wins);
+        System.out.println(" Losses " +losses);
         System.out.println("Ending stake: " + stake);
 
     }
