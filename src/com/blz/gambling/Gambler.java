68b00ead5d97e976641d21ef772a82f93447fc99
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-13 23:41:47 +0530

    ADDED: Resign for the days win or loss

--- src/com/blz/gambling/Gambler.java
+++ src/com/blz/gambling/Gambler.java
@@ -3,19 +3,27 @@ package com.blz.gambling;
 public class Gambler {
     public static void main(String[] args) {
         int stake = 100;
-        int goal = 200;
         int bet = 1;
         int days = 0;
-       for (int i = 0; i < 100 && stake < goal; i++);
-            days++;
+        int wins = 0;
+        int losses = 0;
+
+        for (int i = 0; i < 100; i++) {
+            int originalStake = stake;
             stake = stake - bet;
             boolean win = Math.random() < 0.5;
             if (win) {
                 stake = stake + bet;
+                wins++;
+            } else {
+                losses++;
             }
-        System.out.println("Days taken to reach goal: " + days);
-        System.out.println("Ending stake: " + stake);
-
-    }
-
+            if (stake <= originalStake / 2) {
+                break;
+            }
+        }
+        System.out.println("Resign for the day " + stake);
+            System.out.println(" Wins " + wins);
+            System.out.println(" Losses " + losses);
+        }
     }
