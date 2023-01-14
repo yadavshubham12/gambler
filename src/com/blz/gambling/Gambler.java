c47f87f09f26239adb074e28b3a41ae5f263f2fc
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-14 13:49:11 +0530

    ADDED: monthly days won and loss

--- src/com/blz/gambling/Gambler.java
+++ src/com/blz/gambling/Gambler.java
@@ -3,32 +3,39 @@ package com.blz.gambling;
 public class Gambler {
     public static void main(String[] args) {
         int stake = 100;
-        int days = 20;
         int bet = 1;
         int totalWon = 0;
         int totalLoss = 0;
+        int dayswon = 0;
+        int daysloss = 0;
         int won = 0;
         int loss = 0;
 
-        for (int i = 0; i < 20; i++) {
-            days++;
-          for (int j = 0; j < 100; j++){
-              stake = stake - bet;
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 100; j++) {
+                stake = stake - bet;
                 boolean win = Math.random() < 0.5;
                 if (win) {
                     stake = stake + bet;
                     won = won + bet;
-                    totalWon = totalWon + won;
                 } else {
                     loss = loss + bet;
-                    totalLoss = totalLoss + loss;
                 }
-                    stake = 100;
+                if (won > loss) {
+                    dayswon++;
+                    totalWon += won;
+                } else {
+                    daysloss++;
+                    totalLoss += loss;
                 }
             }
-        System.out.println("Total days played " + days);
-        System.out.println(" Total amount won " + totalWon);
-        System.out.println(" Total amount Loss " + totalLoss);
+                stake = 100;
+
+            }
+            System.out.println("Days Won " + dayswon);
+            System.out.println("Days Loss " + daysloss);
+            System.out.println(" Total amount won: $ " + totalWon);
+            System.out.println(" Total amount Loss: $ " + totalLoss);
         }
 
     }
